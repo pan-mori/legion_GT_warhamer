7937fa11a132ac94f8c377fb96992f436037d3ab
Pan Mori <pan-mori@protonmail.com>
2025-11-11 23:09:45 +0100

    MVP

---
+++ todomapinfo.mk
@@ -0,0 +1,78 @@
+map1:
+TAKE AND HOLD
+Several strategic locations have been identified in your vicinity. Your orders are to assault these positions, secure them and hold them at any cost.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the Command phase (or the end of your turn if it is the fifth battle round and you are going second).
+
+The player whose turn it is scores 5VP for each objective marker they control (up to 15VP per turn).
+
+map2:
+PURGE THE FOE 
+Exterminate the enemy. Show them no mercy.
+
+ANY BATTLE ROUND
+WHEN: End of the battle round.
+
+Each player scores 4VP if one or more enemy units were destroyed this battle round.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the battle round.
+
+Each player scores 4VP if more enemy units than friendly units were destroyed this battle round.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the Command phase (or the end of your turn if it is the fifth battle round and you are going second).
+
+The player whose turn it is scores 4VP if they control one or more objective markers, and an additional 4VP if they control more objective markers than their opponent controls.
+
+map3:
+LINCHPIN
+True victory is built upon a firm foundation. If you cannot hold the centre, then all else will crumble swiftly.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the Command phase (or the end of your turn if it is the fifth battle round and you are going second).
+
+If the player whose turn it is does not control the objective marker in their deployment zone, they score 3VP for each objective marker they control.
+
+OR
+If the player whose turn it is controls the objective marker in their deployment zone, they score 3VP for controlling that objective marker, and 5VP for each other objective marker they control (up to 15VP per turn).
+map 4:
+SCORCHED EARTH
+What cannot be secured must be burned to ash.
+
+BURN OBJECTIVE (ACTION)
+STARTS: Your Shooting phase, from the second battle round onwards.
+
+UNITS: One unit from your army within range of an objective marker that is not within your deployment zone.
+
+COMPLETES: End of your opponent’s next turn or the end of the battle (whichever comes first), if your unit is still within range of the same objective marker and you control that objective marker.
+
+IF COMPLETED: That objective marker is burned and removed from the battlefield.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: Any time.
+
+Each time a player burns an objective marker, that player scores 5VP if that objective marker was in No Man’s Land, or 10VP instead if that objective marker was in their opponent’s deployment zone.
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the Command phase (or the end of your turn if it is the fifth battle round and you are going second).
+
+The player whose turn it is scores 5VP for each objective marker they control (up to 10VP per turn).
+
+map 5:
+HIDDEN SUPPLIES
+Reconnaissance units have uncovered a hidden cache of materiel in this war zone.
+
+In the Place Objective Markers step, players must set up one additional objective marker in No Man’s Land.
+
+Before setting up this new objective marker, players must first move the objective marker in the centre of the battlefield 6" directly towards one of the corners of the battlefield (if No Man’s Land touches any of the corners of the battlefield, you must move the objective marker towards one of those corners). Otherwise, the players roll off, and the winner selects which corner the objective marker is moved towards. Players then set up the new objective marker 6 from the centre of the battlefield towards the diagonally opposite corner of the battlefield to the previously moved objective marker.
+
+
+SECOND BATTLE ROUND ONWARDS
+WHEN: End of the Command phase (or the end of your turn if it is the fifth battle round and you are going second).
+
+The player whose turn it is scores VP as follows (these are cumulative):
+5VP If they control one objective marker not within their deployment zone.
+5VP If they control two objective markers not within their deployment zone.
+5VP if they control more objective markers than their opponent controls.
